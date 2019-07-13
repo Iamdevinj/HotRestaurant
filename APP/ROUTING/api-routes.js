120aa2ffad894c6b8e7631f6dd051098cc8c8e4e
Devin Jenkins <dvnjenks@gmail.com>
2019-07-13 13:20:47 -0700

    added jumbotron

---
+++ APP/ROUTING/api-routes.js
@@ -0,0 +1,9 @@
+var tableData = require("../DATA/table-data");
+var waitlistData = require("../DATA/waitinglist-data");
+
+module.exports = function(app) {
+  app.get("/api/tables", function(req, res) {});
+  app.get("/api/waitlist", function(req, res) {});
+  app.get("/api/tables", function(req, res) {});
+  app.get("/api/clear", function(req, res) {});
+};
