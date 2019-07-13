882afd261a132ede3ba11e4532f4c05dc78657b4
Devin Jenkins <dvnjenks@gmail.com>
2019-07-13 11:43:56 -0700

    added arrays to table and waitlist js files

---
+++ APP/DATA/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+  {
+    customerName: "Keyshia",
+    customerEmail: "hotgirl101@gmail.com",
+    customerID: "key23456",
+    phoneNumber: "555-555-5555"
+  }
+];
+
+module.exports = tableArray;
---
+++ APP/DATA/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitlistArray = [
+  {
+    customerName: "Kanye",
+    customerEmail: "MR.West@gmail.com",
+    customerID: "Kway23456",
+    phoneNumber: "555-555-5555"
+  }
+];
+
+module.exports = waitlistArray;
